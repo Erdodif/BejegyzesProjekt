874ed1251e8452dc7f09610667656aa53ae302c9
Vida Margaréta <margaretavida120@gmail.com>
2021-09-29 14:55:32 +0200

    Bejegyzes osztály megfeleltetése a feladatban megadott UML diagrammnak

--- src/hu/petrik/Bejegyzes.java
+++ src/hu/petrik/Bejegyzes.java
@@ -1,4 +1,56 @@
 package hu.petrik;
 
+import java.time.LocalDateTime;
+
 public class Bejegyzes {
+    private String szerzo;
+    private String tartalom;
+    private int likeok;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+    public Bejegyzes(String szerzo, String tartalom){
+        letrejott = LocalDateTime.now();
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+        likeok = 0;
+        szerkesztve = letrejott;
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+        szerkesztve = LocalDateTime.now();
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
+    }
+
+    public void like(){
+        this.likeok++;
+    }
+
+    @Override
+    public String toString() {
+        String szerkesztvevolt = (szerkesztve == letrejott) ? ("Szerkesztve: " + szerkesztve): "";
+        return szerzo + " - " + likeok + " - " + letrejott +'\n'+
+                szerkesztvevolt +'\n'+
+                tartalom;
+    }
 }
